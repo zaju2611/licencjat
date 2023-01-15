351a3196368e0e41a2e1017754c17d119ced8cc6
Jakub Zając <jakub.zajac2611@gmail.com>
2023-01-15 12:46:46 +0100

    strona startowa

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const footerYear = document.querySelector(".footer__year");
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
