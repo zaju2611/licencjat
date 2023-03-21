d3eb66fc19e6354fae2d7f692722d74e7a0abfae
Jakub Zając <jakub.zajac2611@gmail.com>
2023-03-21 19:02:11 +0100

    added new navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,21 @@
 const footerYear = document.querySelector(".footer_year");
+const burgerButton = document.querySelector(".nav_container-burgerButton");
+const nav = document.querySelector(".nav_container-list");
+const navitems = document.querySelectorAll("nav_container-list-item");
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 
-handleCurrentYear();
+burgerButton.addEventListener("click", () => {
+	nav.classList.add("active");
+});
 
+navitems.forEach((item) => {
+	item.addEventListener("click", () => {
+		nav.classList.remove("active");
+	});
+});
+
+handleCurrentYear();
